e8fe332706aee414c7f88ee616910c4403f78683
joriskian <jo_risk@hotmail.com>
2023-04-24 20:18:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="wrapper">
+        <div class="div1"></div>
+        <div class="div2"></div>
+        <div class="div3"></div>
+        <div class="div4"></div>
+        <div class="div5"></div>
+        <div class="div6"></div>
+        <div class="div7"></div>
+        <div class="div8"></div>
+        <div class="div9"></div>
+        <div class="div10"></div>
+        <div class="div11"></div>
+        <div class="div12"></div>
+        <div class="div13"></div>
+        <div class="div14"></div>
+        <div class="div15"></div>
+        <div class="div16">
+          <h1>Titre de l'appli</h1>
+          <p>
+            Lorem nulla tempor anim consequat cupidatat cillum qui exercitation
+            ut Lorem reprehenderit ut. Ex deserunt nisi minim mollit mollit
+            ullamco esse mollit. Ipsum in enim aliquip mollit ea aute aliquip
+            nostrud. Laboris est duis dolore deserunt consequat ullamco proident
+            elit mollit voluptate. Anim pariatur minim amet laboris. Sit laborum
+            minim nostrud tempor reprehenderit. Laboris irure aute exercitation
+            deserunt sunt.
+          </p>
+        </div>
+        <div class="div17">
+          <p>
+            Tempor quis Lorem Lorem aliqua et. Cupidatat ullamco mollit Lorem
+            enim reprehenderit dolore in ullamco eu eiusmod non nulla. Nisi
+            incididunt velit occaecat voluptate est reprehenderit consequat
+            sunt.
+          </p>
+        </div>
+        <div class="div18"></div>
+      </div>
     </div>
   );
 }
