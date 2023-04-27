a635abe05e516f6c40d253f6407f1af0e28d531e
joriskian <jo_risk@hotmail.com>
2023-04-27 15:09:39 +0200

    add width and hight to train.svg | fixed the responsive

---
+++ src/components/First.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function First() {
+  return (
+    <div className="App">
+      <div class="wrapper">
+        <div class="div1"></div>
+        <div class="div2"></div>
+        <div class="div3"></div>
+        <div class="div4"></div>
+        <div class="div5"></div>
+        <div class="div6"></div>
+        <div class="div7"></div>
+        <div class="div8">
+          <h1>WELCOME</h1>
+        </div>
+        <div class="div9"></div>
+        <div class="div10 vertical"></div>
+        <div class="div11"></div>
+        <div class="div12 vertical"></div>
+        <div class="div13"></div>
+        <div class="div14 vertical"></div>
+        <div class="div15"></div>
+        <div class="div16">
+          <h1>LOREM IPSUM NULLA TEMPOR</h1>
+          <p>
+            Lorem nulla tempor anim consequat cupidatat cillum qui exercitation
+            ut Lorem reprehenderit ut. Ex deserunt nisi minim mollit mollit
+            ullamco esse mollit. Ipsum in enim aliquip mollit ea aute aliquip
+            nostrud. Laboris est duis dolore deserunt consequat ullamco proident
+            elit mollit voluptate. Anim pariatur minim amet laboris. Sit laborum
+            minim nostrud tempor reprehenderit. Laboris irure aute exercitation
+            deserunt sunt.
+          </p>
+        </div>
+        <div class="div17">
+          <div class="flex">
+            <p>
+              Tempor quis Lorem Lorem aliqua et. Cupidatat ullamco mollit Lorem
+              enim reprehenderit dolore in ullamco eu eiusmod non nulla. Nisi
+              incididunt velit occaecat voluptate est reprehenderit consequat
+              sunt.
+            </p>
+            <p>
+              Tempor quis Lorem Lorem aliqua et. Cupidatat ullamco mollit Lorem
+              enim reprehenderit dolore in ullamco eu eiusmod non nulla. Nisi
+              incididunt velit occaecat voluptate est reprehenderit consequat
+              sunt.
+            </p>
+            <p>
+              Tempor quis Lorem Lorem aliqua et. Cupidatat ullamco mollit Lorem
+              enim reprehenderit dolore in ullamco eu eiusmod non nulla. Nisi
+              incididunt velit occaecat voluptate est reprehenderit consequat
+              sunt.
+            </p>
+          </div>
+        </div>
+        <div class="div18">
+          <p>Here we are at our destination</p>
+        </div>
+        <div class="div19">
+          <div class="train"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default First
---
+++ src/components/Second.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styles from './Second.module.css';
+
+function Second() {
+  return (
+    <div className={styles.angry_grid}>
+      <div className={styles.item_0}>&nbsp;</div>
+      <div className={styles.item_1}>&nbsp;</div>
+      <div className={styles.item_2}>&nbsp;</div>
+      <div className={styles.item_3}>&nbsp;</div>
+      <div className={styles.item_4 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_5 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_6 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_7 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_8 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_9 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_10 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_11 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_12 + ' ' + styles.virage}>&nbsp;</div>
+      <div className={styles.item_13 + ' ' + styles.verticale}>&nbsp;</div>
+      <div className={styles.item_14 + ' ' + styles.verticale}>&nbsp;</div>
+      <div className={styles.item_15 + ' ' + styles.verticale}>&nbsp;</div>
+      <div className={styles.item_16 + ' ' + styles.horizontale}>&nbsp;</div>
+      <div className={styles.item_17 + ' ' + styles.horizontale}>&nbsp;</div>
+      <div className={styles.item_18 + ' ' + styles.horizontale}>&nbsp;</div>
+      <div className={styles.item_19 + ' ' + styles.horizontale}>&nbsp;</div>
+      <div className={styles.item_20 + ' ' + styles.verticale}>&nbsp;</div>
+      <div className={styles.item_21}>
+        <div></div>
+      </div>
+      <div className={styles.item_22}>&nbsp;</div>
+      <div className={styles.item_23}>
+        <h1>WELCOME</h1>
+      </div>
+      <div className={styles.item_24}>
+        <p>
+          Est eiusmod culpa elit adipisicing et. Ea veniam veniam aute consequat
+          mollit voluptate. Elit qui dolor magna ut ad sint ipsum duis ipsum.
+          Cupidatat pariatur adipisicing proident nostrud nisi. Ipsum ad labore
+          nostrud culpa minim anim aute aliqua nisi in laborum ipsum aliqua
+          exercitation. Ad incididunt anim sint aliquip officia ipsum. Et duis
+          duis qui consectetur eu est.
+        </p>
+      </div>
+      <div className={styles.item_25}>
+        <p>
+          Est eiusmod culpa elit adipisicing et. Ea veniam veniam aute consequat
+          mollit voluptate. Elit qui dolor magna ut ad sint ipsum duis ipsum.
+          Cupidatat pariatur adipisicing proident nostrud nisi. Ipsum ad labore
+          nostrud culpa minim anim aute aliqua nisi in laborum ipsum aliqua
+          exercitation. Ad incididunt anim sint aliquip officia ipsum. Et duis
+          duis qui consectetur eu est.
+        </p>
+      </div>
+      <div className={styles.item_26}>
+        <p>
+          Est eiusmod culpa elit adipisicing et. Ea veniam veniam aute consequat
+          mollit voluptate. Elit qui dolor magna ut ad sint ipsum duis ipsum.
+          Cupidatat pariatur adipisicing proident nostrud nisi. Ipsum ad labore
+          nostrud culpa minim anim aute aliqua nisi in laborum ipsum aliqua
+          exercitation. Ad incididunt anim sint aliquip officia ipsum. Et duis
+          duis qui consectetur eu est.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Second;
